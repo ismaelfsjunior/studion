c3594b734abb806ca5fbeb500eb4cb3479ada156
ismael junior <ifsjunior2@gmail.com>
2022-10-18 10:27:44 -0300

    feat(NAVBAR): Reponsividade menu.

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,10 @@
 $(document).ready( () => {
+  // menu
+    $('.navbar__menu-btn').on('click', function () {
+        $('.navbar__links').toggleClass('active');
+        $(this).find('i').toggleClass("fa-bars");
+        $(this).find('i').toggleClass("fa-square-xmark");
+    });
     const slickOptions =  {
         autoplay: true,         
         dots: false,  
