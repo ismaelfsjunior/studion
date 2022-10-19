2fba6293a145e3551e99c79bb84871f63d91087c
ismael junior <ifsjunior2@gmail.com>
2022-10-19 20:10:42 -0300

    feat(COMPANY): Responsividade mobile page empresa.

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,4 @@
 $(document).ready( () => {
-  // menu
-    $('.navbar__menu-btn').on('click', function () {
-        $('.navbar__links').toggleClass('active');
-        $(this).find('i').toggleClass("fa-bars");
-        $(this).find('i').toggleClass("fa-square-xmark");
-    });
     const slickOptions =  {
         autoplay: true,         
         dots: false,  
